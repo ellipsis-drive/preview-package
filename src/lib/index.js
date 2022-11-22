d903fc3758a419100a7eca3ebe023aeb09f4abe7
Floyd Remmerswaal <floydremmerswaal@gmail.com>
2022-11-22 19:38:02 +0100

    get metadata

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,6 +5,71 @@ let apiCall = (path, body, user, cb) => {
 };
 
 class EllipsisPreview {
+  getButton = (text, cb) => {
+    let button = document.createElement("button");
+    button.onclick = cb;
+    button.innerHTML = text;
+    button.style.color = "#fff";
+    button.style.backgroundColor = "#089EC8";
+    button.style.boxShadow =
+      "0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12)";
+    button.style.padding = "6px 16px";
+    button.style.fontSize = "1rem";
+    button.style.minWidth = "64px";
+    button.style.boxSizing = "border-box";
+    button.style.transition = `"background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms`;
+    button.style.fontFamily = [
+      "Roboto Condensed",
+      "Roboto",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "Segoe UI",
+      "Helvetica Neue",
+      "Arial",
+      "sans-serif",
+      "Apple Color Emoji",
+      "Segoe UI Emoji",
+      "Segoe UI Symbol",
+      "Exo 2",
+    ];
+    button.style.fontWeight = "500";
+    button.style.lineHeight = "1.75";
+    button.style.borderRadius = "4px";
+    button.style.letterSpacing = "0.5px";
+    button.style.textTransform = "uppercase";
+    button.style.border = 0;
+    return button;
+  };
+
+  loginDiv = () => {
+    let div = document.createElement("div");
+    div.appendChild(this.p("Please log in to your Ellipsis Drive account:"));
+    div.style.textAlign = "center";
+    let button = this.getButton("Log in", () => {
+      window.location = `https://app.ellipsis-drive.com/login?referer=${window.location.href}`;
+    });
+    div.appendChild(button);
+    return div;
+  };
+
+  getMetaData = async (pathId) => {
+    let headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${this.settings.token}`,
+    };
+
+    let request = await fetch(`${API}/path/${pathId}`, {
+      method: "GET",
+      headers: headers,
+    });
+
+    return request.json().then((ret) => {
+      this.layerLoaded = true;
+      this.layer = ret;
+      this.render();
+    });
+  };
+
   computeRatio = (extent) =>
     (extent.xMax - extent.xMin) / (extent.yMax - extent.yMin);
 
@@ -73,12 +138,18 @@ class EllipsisPreview {
   };
 
   settings = {};
+  layer = null;
+  layerLoaded = false;
 
   constructor(options = {}) {
     this.settings = { ...this.defaultSettings, ...options };
 
     if (!("div" in options)) {
       console.warn("EllipsisPreview: no div is provided!");
+      return;
+    }
+    if (!("pathId" in options)) {
+      console.warn("EllipsisPreview: no pathId is provided!");
       return;
     }
 
@@ -94,6 +165,9 @@ class EllipsisPreview {
         this.settings.loggedIn = true;
       }
     }
+
+    this.getMetaData(this.settings.pathId);
+
     this.render();
   }
 
@@ -105,17 +179,24 @@ class EllipsisPreview {
     return elem;
   };
 
-
   render = () => {
+    this.settings.div.innerHTML = "";
+    if (this.settings.loggedIn) {
+      let div = document.createElement("div");
+      let p = this.p("Ellipsis Preview");
+      let pathId = this.p(this.settings.pathId);
 
-    let div = document.createElement("div");
-    let p = this.p("Ellipsis Preview")
-    let pathId = this.p(this.settings.pathId);
+      div.appendChild(p);
+      div.appendChild(pathId);
+      if (this.layerLoaded) {
+        console.log("layer loaded!");
+        console.log(this.layer);
+      }
 
-    div.appendChild(p)
-    div.appendChild(pathId);
-
-    this.settings.div.appendChild(div);
+      this.settings.div.appendChild(div);
+    } else {
+      this.settings.div.appendChild(this.loginDiv());
+    }
   };
 }
 
