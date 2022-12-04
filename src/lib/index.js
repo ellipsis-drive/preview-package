876a3cb03d1872fbc019294c120a230431c35a7a
Floyd Remmerswaal <floydremmerswaal@gmail.com>
2022-12-04 16:30:33 +0100

    work on getting the base image to work

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,5 @@
+import proj4 from "./proj4";
+
 const API = "https://api.ellipsis-drive.com/v3/";
 
 let apiCall = (path, body, user, cb) => {
@@ -5,6 +7,52 @@ let apiCall = (path, body, user, cb) => {
 };
 
 class EllipsisPreview {
+
+  isValidTimestamp = (t) => {
+    if (t.status !== 'active') {
+      return { available: false, reason: 'Timestamp not active' };
+    } else if (t.availability.blocked) {
+      return { available: false, reason: t.availability.reason };
+    }
+    return { available: true };
+  };
+  
+  isValidMap = (m) => {
+    if (!m) {
+      return { available: false, reason: 'No Layer' };
+    }
+    if (m.type !== 'raster' && m.type !== 'vector') {
+      return { available: true };
+    }
+    if (m.disabled) {
+      return { available: false, reason: 'Layer disabled' };
+    }
+    if (m.deleted) {
+      return { available: false, reason: 'Layer trashed' };
+    }
+    if (m.yourAccess.accessLevel === 0) {
+      return { available: false, reason: 'No access' };
+    }
+    if (m[m.type].timestamps.filter((t) => this.isValidTimestamp(t, m).available).length === 0) {
+      if (m[m.type].timestamps.find((t) => t.availability?.reason === 'relocation')) {
+        return { available: false, reason: 'Relocating layer' };
+      } else if (m[m.type].timestamps.find((t) => t.availability?.reason === 'reindexing')) {
+        return { available: false, reason: 'Reindexing layer' };
+      } else if (m.type === 'raster' && m[m.type].timestamps.filter((t) => t.uploads.completed > 0).length === 0) {
+        return { available: false, reason: 'No uploads' };
+      } else if (m[m.type].timestamps.find((t) => t.status === 'activating')) {
+        return { available: false, reason: 'Activating files' };
+      } else if (m[m.type].timestamps.find((t) => t.status === 'pausing')) {
+        return { available: false, reason: 'Pausing files' };
+      } else if (m[m.type].timestamps.find((t) => t.status === 'created')) {
+        return { available: false, reason: 'No active timestamps' };
+      } else {
+        return { available: false, reason: 'No timestamps' };
+      }
+    }
+    return { available: true };
+  };
+
   getButton = (text, cb) => {
     let button = document.createElement("button");
     button.onclick = cb;
@@ -74,7 +122,7 @@ class EllipsisPreview {
     (extent.xMax - extent.xMin) / (extent.yMax - extent.yMin);
 
   getNewExtent = (goalAspectRatio, extent) => {
-    const currentAspectRatio = computeRatio(extent);
+    const currentAspectRatio = this.computeRatio(extent);
     if (goalAspectRatio > currentAspectRatio) {
       const widthToAdd =
         ((extent.xMax - extent.xMin) * (goalAspectRatio - currentAspectRatio)) /
@@ -99,10 +147,23 @@ class EllipsisPreview {
     }
   };
 
+  getReprojectedExtent = (extent) => {
+    const reprojectedMins = proj4('EPSG:4326', 'EPSG:3857', [extent.xMin, extent.yMin]);
+  
+    const reprojectedMaxs = proj4('EPSG:4326', 'EPSG:3857', [extent.xMax, extent.yMax]);
+  
+    return {
+      xMin: reprojectedMins[0],
+      yMin: reprojectedMins[1],
+      xMax: reprojectedMaxs[0],
+      yMax: reprojectedMaxs[1],
+    };
+  };
+
   getBaseMapPng = ({ extent, height, width }) => {
-    const newExtent = getNewExtent(
+    const newExtent = this.getNewExtent(
       width / height,
-      getReprojectedExtent(extent)
+      this.getReprojectedExtent(extent)
     );
 
     return `https://ows.mundialis.de/osm/service?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&BBOX=${
@@ -114,27 +175,28 @@ class EllipsisPreview {
     )}&LAYERS=OSM-WMS-no-labels&STYLES=&FORMAT=image/png&DPI=96&MAP_RESOLUTION=96&FORMAT_OPTIONS=dpi:96&TRANSPARENT=true`;
   };
 
-  getEllipsisMapPng = ({
-    mapId,
-    extent,
-    timestampId,
-    layerId,
-    width,
-    height,
-    token,
-  }) => {
-    url = `/ogc/wms/${mapId}?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&BBOX=${extent}&SRS=EPSG:3857&width=${width}&height=${height}&LAYERS=${timestampId}_${styleId}&STYLES=&FORMAT=image/png&DPI=96&MAP_RESOLUTION=96&FORMAT_OPTIONS=dpi:96&TRANSPARENT=TRUE`;
+  getEllipsisMapPng = ({ mapId, extent, timestampId, styleId, width, height, token }) => {
     if (token) {
-      url += `&token=${token}`;
+      return `v3/ogc/wms/${mapId}?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&BBOX=${
+        extent.xMin
+      },${extent.yMin},${extent.xMax},${
+        extent.yMax
+      }&SRS=EPSG:3857&width=${width}&height=${height}&LAYERS=${timestampId}_${styleId}&STYLES=&FORMAT=image/png&DPI=96&MAP_RESOLUTION=96&FORMAT_OPTIONS=dpi:96&TRANSPARENT=TRUE&token=${token}`;
+    } else {
+      return `v3/ogc/wms/${mapId}?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&BBOX=${
+        extent.xMin
+      },${extent.yMin},${extent.xMax},${
+        extent.yMax
+      }&SRS=EPSG:3857&width=${width}&height=${height}&LAYERS=${timestampId}_${styleId}&STYLES=&FORMAT=image/png&DPI=96&MAP_RESOLUTION=96&FORMAT_OPTIONS=dpi:96&TRANSPARENT=TRUE`;
     }
-
-    return url;
   };
 
   defaultSettings = {
     div: null,
     token: null,
     pathId: null,
+    timestampId: null,
+    styleId: null,
   };
 
   settings = {};
@@ -179,21 +241,128 @@ class EllipsisPreview {
     return elem;
   };
 
+  getPlaceholderImg = () => {
+    let placeholder = document.createElement("div");
+    placeholder.style.backgroundImage = "url(https://app.ellipsis-drive.com/images/drive/map_placeholder.jpg)";
+    placeholder.style.width = "inherit";
+    placeholder.style.height = "inherit";
+    placeholder.style.gap = "12px";
+    placeholder.style.top = "0";
+    placeholder.style.left = "0";
+    placeholder.style.right = "0";
+    placeholder.style.bottom = "0";
+    placeholder.style.display = "flex";;
+    placeholder.style.alignItems = "center";
+    placeholder.style.flexDirection= "column";
+    placeholder.style.backgroundSize = "contain";
+    placeholder.style.justifyContent = "center";
+    placeholder.style.backgroundImage = "url(https://app.ellipsis-drive.com/images/drive/map_placeholder.jpg)";
+    placeholder.style.backgroundPosition = "center";
+    return placeholder;
+  }
+
+  getExtent = (layer) => {
+    // TODO for now we just pick the first one, but some more elaborate picking scheme should be implemented later
+    let type = layer.type;
+    return {extent: layer[type].timestamps[0].extent, timestampId: layer[type].timestamps[0].id, styleId: layer[type].styles[0].id};
+
+    if (this.settings.timestampId === null){
+      // find our own, if that fails we render placeholder
+
+      
+
+    } else {
+      timestampId = this.settings.timestampId
+    }
+    return layer[type].timestamps[timestampId].extent
+  }
+
+  requestEllipsisPng = () =>{
+    let obj = {
+      extent: this.getExtent(this.layer).extent,
+      width: WIDTH,
+      height: HEIGHT,
+      token: this.settings.token,
+      timestampId: this.getExtent(this.layer).timestampId,
+      styleId: this.getExtent(this.layer).styleId,
+      mapId: this.layer.id,
+    }
+    let ellipsispng = this.getEllipsisMapPng(obj);
+
+    let request = await fetch(`${API}${ellipsispng}}`, {
+      method: "GET",
+      headers: headers,
+    });
+  }
+
+  requestBasePng = () => {
+    let obj = {
+      extent: this.getExtent(this.layer).extent,
+      width: WIDTH,
+      height: HEIGHT,
+      token: this.settings.token,
+      timestampId: this.getExtent(this.layer).timestampId,
+      styleId: this.getExtent(this.layer).styleId,
+      mapId: this.layer.id,
+    }
+
+    let basepng = this.getBaseMapPng(obj);
+  }
+
+  previewRender = () => {
+
+    const WIDTH = "300";
+    const HEIGHT = "200"; 
+
+    let div = document.createElement("div");
+    console.log(this.layer);
+
+
+    let imgdiv = document.createElement("div");
+    imgdiv.style.width = WIDTH;
+    imgdiv.style.height = HEIGHT;
+
+    let placeholder = this.getPlaceholderImg();
+    imgdiv.appendChild(placeholder);
+
+    let obj = {
+      extent: this.getExtent(this.layer).extent,
+      width: WIDTH,
+      height: HEIGHT,
+      token: this.settings.token,
+      timestampId: this.getExtent(this.layer).timestampId,
+      styleId: this.getExtent(this.layer).styleId,
+      mapId: this.layer.id,
+    }
+
+    let basepng = this.getBaseMapPng(obj);
+    let ellipsispng = this.getEllipsisMapPng(obj);
+
+    console.log(basepng);
+    console.log(ellipsispng);
+
+    div.appendChild(this.p(this.layer.name));
+    div.appendChild(this.p(this.settings.pathId));
+    div.appendChild(imgdiv);
+
+    return div;
+  }
+
+  loadingRender = () => {
+    // should at some point just render the same as previewrender, but grayed out
+    let div = document.createElement("div");
+    div.appendChild(this.p("Loading..."));
+    return div;
+  }
+
   render = () => {
     this.settings.div.innerHTML = "";
     if (this.settings.loggedIn) {
-      let div = document.createElement("div");
-      let p = this.p("Ellipsis Preview");
-      let pathId = this.p(this.settings.pathId);
-
-      div.appendChild(p);
-      div.appendChild(pathId);
       if (this.layerLoaded) {
-        console.log("layer loaded!");
-        console.log(this.layer);
+        this.settings.div.appendChild(this.previewRender());
+      } else {
+        this.settings.div.appendChild(this.loadingRender());
       }
-
-      this.settings.div.appendChild(div);
     } else {
       this.settings.div.appendChild(this.loginDiv());
     }
