6824932e7a5375aa69ec04710296f65e8588a729
Floyd Remmerswaal <floydremmerswaal@gmail.com>
2023-01-29 20:28:56 +0100

    implement cb call and change cursor to pointer

--- src/test/index.js
+++ src/test/index.js
@@ -2,12 +2,18 @@ import { EllipsisPreview } from "../lib";
 
 let div = document.getElementById("test");
 
+let callback = (layerobj) => {
+  console.log("Clicked on layer");
+  console.log(layerobj);
+}
+
 let options = {
   div: div,
   // pathId: "0ec49fb8-f577-45de-8e4f-6243fdc62908" // volcano_rgb,
    pathId: "0a2362a6-85e8-480a-ade0-a8f2e45f4e0d", // vulcano
   // pathId: "c955e15e-8747-4714-9008-92c0da514c72", // my own\
   // pathId: "2109c37a-d549-45dd-858e-7eddf1bd7c22", // vector map (Basis registratie percelen)
+  cb: callback,
 };
 
 let preview = new EllipsisPreview(options);
